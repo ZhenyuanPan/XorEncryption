ad87cf698f7540bc085f93eb3bb48980ca284aaa
zhenyuan Pan <1656922908@qq.com>
2020-08-20 11:29:37 +0800

    添加项目文件。

---
+++ 简单异或加密/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 简单异或加密
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
